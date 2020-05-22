5118b9b38325f82a9b8017952fe0c528f2859d6d
JaxkDev <gangnam253@gmail.com>
2020-05-22 12:15:20 +0100

    Download binary's when needed & not present.

--- convert.py
+++ convert.py
@@ -1,13 +1,54 @@
 # Convert mediaboxHD m3u8 media to mp4.
 
-import os, platform, sys
-from tkinter import filedialog
+from tkinter import Tk, ttk, filedialog
+from urllib.request import urlretrieve
+from threading import Event, Thread
+import os, platform, sys, time
+from shutil import which
+
+def download(url, filename):
+    global TEMP_1
+    TEMP_1 = filename
+    def reporthook(count, blocksize, totalsize):
+        global start_time
+        if count == 0:
+            start_time = time.time()
+            return
+        duration = time.time() - start_time
+        progress_size = int(count * blocksize)
+        speed = int(progress_size / (1024 * duration))
+        percent = int(count * blocksize * 100 / totalsize)
+        if percent >= 100:
+            if totalsize > (1024*1024):
+                sys.stdout.write(("\r[%s] %i MB downloaded in %i seconds." + (" "*50) + "\n") %
+                        (TEMP_1, progress_size / (1024 * 1024), duration))
+            else:
+                sys.stdout.write(("\r[%s] <1 MB downloaded." + (" "*50) + "\n") % (TEMP_1))
+        else:
+            sys.stdout.write("\r[%s] %d%%, %d MB, %d KB/s, %d seconds elapsed" %
+                        (TEMP_1, percent, progress_size / (1024 * 1024), speed, duration))
+        sys.stdout.flush()
+
+    return urlretrieve(url, filename, reporthook)
 
 system = platform.system()
 cwd = os.path.dirname(os.path.realpath(__file__))
-if system == "Windows":
+if not os.path.exists("bin"):
+    os.mkdir("bin")
+if which("ffmpeg"):
+    binary = "ffmpeg"
+    print("Detected ffmpeg already in PATH.")
+elif system == "Windows":
+    if not os.path.exists(cwd+"\\bin\\ffmpeg-win64.exe"):
+        print("Binary not found for windows, downloading now...")
+        download('https://github.com/JaxkDev/MediaBox-Converter/raw/master/bin/ffmpeg-win64.exe', 'bin/ffmpeg-win64.exe')
+        download('https://github.com/JaxkDev/MediaBox-Converter/raw/master/bin/LICENSE.txt', 'bin/LICENSE.txt')
     binary = cwd + "\\bin\\ffmpeg-win64.exe"
 elif system == "Linux":
+    if not os.path.exists(cwd+"\\bin\\ffmpeg-amd64"):
+        print("Binary not found for linux, downloading now...")
+        download('https://github.com/JaxkDev/MediaBox-Converter/raw/master/bin/ffmpeg-amd64', 'bin/ffmpeg-amd64')
+        download('https://github.com/JaxkDev/MediaBox-Converter/raw/master/bin/LICENSE.txt', 'bin/LICENSE.txt')
     binary = cwd + "\\bin\\ffmpeg-amd64"
 else:
     sys.stderr.write("Unsupported system: "+system)
